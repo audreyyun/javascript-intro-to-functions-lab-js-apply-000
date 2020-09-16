a90d6aabe1c667a2865bfe7ccced8737367afafe
Audrey Yun <96audreyyun@gmail.com>
2020-09-16 20:13:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout (string) {
   return string.toUpperCase();
 }
+
+function whipser (string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
